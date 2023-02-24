91be56001e43b4797fe1d72dad39eb766b59bbf3
t9fiction <sohail.sohailishaq@gmail.com>
2023-02-24 19:06:28 +0500

    landing page without editing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
+import Features from "@/components/Features";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+import Statistics from "@/components/Statistics";
+import Testimonials from "@/components/Testimonials";
 import { Inter } from "@next/font/google";
 // import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <HeroSection />
+      <Features />
+      <Statistics />
+      <Testimonials />
+      <Footer />
+    </>
+  );
 }
--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,9 +1,131 @@
-import React from 'react'
+'use client';
+import { ReactNode } from 'react';
+import {
+  Stack,
+  Container,
+  Box,
+  Flex,
+  Text,
+  Heading,
+  SimpleGrid,
+  useColorModeValue
+} from '@chakra-ui/react';
 
-const Features = () => {
+export default function Features() {
+  const bgColor = useColorModeValue("gray.800", "gray.600");
+  const textColor = useColorModeValue("white", "black");
   return (
-    <div>Features</div>
-  )
+    <Box bg={bgColor} position={'relative'} >
+      <Flex
+        flex={1}
+        zIndex={0}
+        display={{ base: 'none', sm: 'flex' }}
+        backgroundImage="url('/templates/stats-grid-with-image.png')"
+        backgroundSize={'cover'}
+        backgroundPosition="center"
+        backgroundRepeat="no-repeat"
+        position={'absolute'}
+        width={'50%'}
+        insetY={0}
+        right={0}>
+     
+      </Flex>
+      <Container maxW={'7xl'} zIndex={10} position={'relative'}>
+        <Stack direction={{ base: 'column', lg: 'row' }}>
+          <Stack
+            flex={1}
+            color={'gray.400'}
+            justify={{ lg: 'center' }}
+            py={{ base: 2, md: 4, xl: 8 }}>
+            <Box mb={{ base: 4, sm: 10 }}>
+              <Text
+                fontFamily={'heading'}
+                fontWeight={700}
+                textTransform={'uppercase'}
+                mb={3}
+                fontSize={'xl'}
+                color={'white'}>
+                Technology
+              </Text>
+              <Heading
+                color={'white'}
+                mb={5}
+                fontSize={{ base: '3xl', md: '5xl' }}>
+                21st century agriculture
+              </Heading>
+              <Text fontSize={'xl'} color={'gray.400'}>
+                The NewLife™ technology allows you to monitor your crops and get
+                complete insights at real time. The proprietary
+                software/hardware ecosystem prevents your plants from getting
+                neglected.
+              </Text>
+            </Box>
+
+            <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+              {stats.map((stat) => (
+                <Box key={stat.title}>
+                  <Text
+                    fontFamily={'heading'}
+                    fontSize={'3xl'}
+                    color={'white'}
+                    mb={3}>
+                    {stat.title}
+                  </Text>
+                  <Text fontSize={'xl'} color={'gray.400'}>
+                    {stat.content}
+                  </Text>
+                </Box>
+              ))}
+            </SimpleGrid>
+          </Stack>
+          {/* <Flex flex={1} /> */}
+        </Stack>
+      </Container>
+    </Box>
+  );
 }
 
-export default Features
+const StatsText = ({ children }: { children: ReactNode }) => (
+  <Text as={'span'} fontWeight={700} color={'white'}>
+    {children}
+  </Text>
+);
+
+const stats = [
+  {
+    title: '10+',
+    content: (
+      <>
+        <StatsText>Software modules</StatsText> for detailed monitoring and
+        real-time analytics
+      </>
+    ),
+  },
+  {
+    title: '24/7',
+    content: (
+      <>
+        <StatsText>Analytics</StatsText> enabled right in your dashboard without
+        history limitations
+      </>
+    ),
+  },
+  {
+    title: '13%',
+    content: (
+      <>
+        <StatsText>Farms</StatsText> in North America has chosen NewLife™ as
+        their management solution
+      </>
+    ),
+  },
+  {
+    title: '250M+',
+    content: (
+      <>
+        <StatsText>Plants</StatsText> currently connected and monitored by the
+        NewLife™ software
+      </>
+    ),
+  },
+];
