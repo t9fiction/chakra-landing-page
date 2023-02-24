e107a92318bea75daefa1d67b00b2c6736d09497
t9fiction <sohail.sohailishaq@gmail.com>
2023-02-24 16:49:18 +0500

    logo files added

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,10 +1,10 @@
 export default function Head() {
   return (
     <>
-      <title>Landing Page</title>
+      <title>Numeric Sins</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="description" content="Landing Page with Chakra" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/logo.svg" />
     </>
   )
 }
